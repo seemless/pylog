99d44c54972c603c3fd190d3c328c277ef16f762
Matthew Smith <matt.smith@g2-inc.com>
2012-08-21 18:45:14 -0400

    revamped pylog to utilize generators, memory problems gone.

--- ingest.py
+++ ingest.py
@@ -9,52 +9,49 @@ def main(args):
   print("INFO: Args: " + str(args[1:]))
   for path in args[1:]:
 
-    if os.path.isfile(path):
-
-        success = ingest(path)
-        print(success)
-    else:
-      print("ERROR: Invalid path, first argument must be a file", arg)
-      sys.exit(0)
-
-
+      success = ingest(path)
+      print(success)
+      
 
 def ingest(path):
   time1 = time.time()
   print("INFO: ingesting file: "+ path)
+  counter = 0 
+  gen = pylog.gen_events(path)
+  if gen:
+    for e in gen:
+      counter += 1
   
-  events = pylog.parse_log(path)
-      
-  if not events:
-    events = netpyfense.parse(path)      
+  print ("INFO: there were %d events in %s path" % (counter, path))    
+    #events = netpyfense.parse(path)      
   
-  if events:
+  #if events:
     
-    try:
-      connection = pymongo.Connection('localhost', 27017)
-      collection = connection["dapper"]["events"]  
-    except Exception as e:
-      print("ERROR: in ingest database connection")
-      print(e)
-      return False    
+    #try:
+      #connection = pymongo.Connection('localhost', 27017)
+      #collection = connection["dapper"]["events"]  
+    #except Exception as e:
+      #print("ERROR: in ingest database connection")
+      #print(e)
+      #return False    
     
-    #do all the databas insertion
-    try:
-      print("INFO: Inserting events into db: ", len(events))
-      ids = collection.insert(events)
-    except Exception as e:
-      print("ERROR: in database insertion at file:", path)
-      print(e)
-      return False
+    ##do all the databas insertion
+    #try:
+      #print("INFO: Inserting events into db: ", len(events))
+      #ids = collection.insert(events)
+    #except Exception as e:
+      #print("ERROR: in database insertion at file:", path)
+      #print(e)
+      #return False
     
-    time2 = time.time()
-    delta = time2 - time1
-    print("INFO: events ingested into database: ",len(ids))
-    print("INFO: ingest processing time: ",delta) 
-    print("INFO: ingest time range: ",time1," - ",time2)
-    return len(ids)==len(events)
-  else:
-    return False
+    #time2 = time.time()
+    #delta = time2 - time1
+    #print("INFO: events ingested into database: ",len(ids))
+    #print("INFO: ingest processing time: ",delta) 
+    #print("INFO: ingest time range: ",time1," - ",time2)
+    #return len(ids)==len(events)
+  #else:
+  return False
   
 if __name__=="__main__":
   main(sys.argv)
--- pylog.py
+++ pylog.py
@@ -1,22 +1,86 @@
-import re
-import time
-import codecs
+import re, os, sys, time, codecs
 import carny
 
-def parse_log(path):
-    events = []    
+def test(args):
+    gen_count = 0
+    parse_count = 0
     
+    for path in args:
+        gen = gen_events(path)
+        if gen:
+            for e in gen:
+                gen_count += 1
+        parse_count += len(parse_log(path))
+    print(gen_count,parse_count)
+        
+            
+def gen_events(path):
+    log_dict = get_log_type(path)
+            
+    if os.path.isfile(path) and log_dict is not None:
+        type_used = log_dict["type"]
+        print("INFO: log type is: %s" % type_used)
+        file_name = log_dict["file_name"]    
+        method = log_dict["method"]
+        if log_dict["multiline"]:
+            
+            return method(path, type_used, file_name)            
+        else:
+            pat = log_dict["pattern"]
+            headers = log_dict["headers"]
+            lines = gen_line(path)
+            events = gen_dirty_event(lines, method, pat, headers)
+            return gen_clean(events,type_used, file_name)    
+                   
+def gen_clean(events, used, name):
+    '''Take in an event iterable, log type, and a file name
+and use the latter two to enrich the event and give the event
+an epoch timestamp'''
+
+    for e in events:
+        if e is not None:
+            if used in unspecified:
+                e["date_time"] = e["date_time"].strip()+" 2006"
+            try:
+                e["epoch"] = carny.guess(e["date_time"])
+                                
+            except Exception as e:
+                print("event is mis-formatted")
+                print(e)
+                yield None
+            e['type'] = used
+            e['file_name'] = name
+            yield e   
+            
+def gen_dirty_event(lines, method, pattern, use_headers=None):
+    '''abstaction generator that takes in lines of log files
+their parsing method, the pattern to use, and the fields that
+the line will be parsed into (if necessary). Methods supplied 
+in the "method" argument must return a dictionary or None.'''
+    
+    for line in lines:
+        yield method(line, pattern, use_headers)
+    
+
+def gen_line(path):
+    '''Given a path, yeild each line for further processing. '''
+    with open(path) as f:
+        for line in f:
+            yield line
+    
+def get_log_type(path):
+    '''determine is pylog can parse the file given. There are strict
+naming conventions used in pylog. Utilize the log_types() method to aid
+in your use.'''
     log_dict = None
-    type_used = None
     file_name = path.split("/")[-1]
-    print(file_name)
     
     #find the appropriate log definition for parsing
     for log_type in log_map:
         if log_type in file_name or log_type in file_name.split(".")[0]:
-
+  
             log_dict = log_map[log_type]  
-            type_used = log_type
+            log_dict['type'] = log_type
             #find it and get out before we duplicate (greedy)
             break
         
@@ -24,8 +88,23 @@ def parse_log(path):
     #select the appropriate generic log file parser
     if log_dict is None and file_name.endswith(".log"):
         log_dict = generic_logs['.log']
-        type_used = '.log'
-        
+        log_dict['type'] = '.log' 
+    
+    #If we have the log type, add the file name
+    if log_dict is not None:
+        log_dict['file_name'] = file_name
+    else:
+        print("WARN: Log type %s not found, pylog is ignoring"%file_name)
+    
+    return log_dict
+
+def parse_log(path):
+    events = []    
+    
+    log_dict = None
+    type_used = None
+    file_name = path.split("/")[-1]
+    log_dict = get_log_type(path)
     
     if log_dict is not None:
         
@@ -144,7 +223,7 @@ def parse_mail_log_line(line,pattern,headers=None):
 def parse_ossec_log(path, type_used, file_name):
     
     events = []
-    with codecs.open(path, encoding='utf-8') as f:
+    with open(path) as f:
         for line in f:
             if "**" in line:
                 event = dict()
@@ -167,9 +246,10 @@ def parse_ossec_log(path, type_used, file_name):
                     event[line[:indx].strip()] = line[indx+1:].strip()
                     
             if not line.strip():
-                events.append(event)
-
+                events.extend([event])
+   
     return events
+
 
 def clean(event):
   
@@ -368,3 +448,5 @@ log_map = {
                      },
         
           }
+
+if __name__=="__main__": test(sys.argv[1:])
